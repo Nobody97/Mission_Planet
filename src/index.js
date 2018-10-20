f0bcd535b637687d874b86bb33f323f9243b56e0
Florian Hess <floh.hess97@web.de>
2018-10-20 20:18:48 +0200

    Neu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-"use strict";
+function hide() {
+  document.getElementsById('sidebar').style.display='none';
+};
 
-  import App from "./app.js";
-  import stylesheet from "./index.css";
-
-   // Erst loslaufen, wenn das Document Object Modul bereit ist
-   window.addEventListener("load", () => {
-       // Anwendung starten
-       let app = new App();
-       app.start();
-   });
+function show() {
+  document.getElementsById('sidebar').style.display='block';
+};
