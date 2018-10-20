a9cb0d526e2e733e30fcd4c9cb172f498ede7a8f
Florian Hess <floh.hess97@web.de>
2018-10-20 22:00:22 +0200

    Überarbeitet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-function hide() {
-  document.getElementsById('sidebar').style.display='none';
-};
+function myFunction(e) {
+  e.classList.toggle("show");
 
-function show() {
-  document.getElementsById('sidebar').style.display='block';
-};
+  var element = document.getElementById('navID'),
+  Style = window.getComputedStyle(element),
+  right = Style.getPropertyValue("right");
+
+  if(right == "0px") {
+    element.style.rigth = "-260px";
+  }else {
+    element.style.left = "0px";
+  }
+}
